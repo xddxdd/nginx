0f3d19e6e25e89ad7ef7380239c73bd54935dd4b
Lan Tian <xuyh0120@outlook.com>
2026-01-31 15:41:35 -0800

    Add nix development shell

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "nginx development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              gcc
+              gnumake
+              pkg-config
+            ];
+
+            buildInputs = with pkgs; [
+              openssl
+              pcre2
+              zlib
+              libxcrypt
+              gdb
+            ];
+          };
+        }
+      );
+    };
+}
